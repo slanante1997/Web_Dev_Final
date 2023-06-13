82c0df70f73670722373607eaf984955c7a088fe
mmoran314 <mmoran9@ewu.edu>
2023-06-13 14:56:55 -0700

    Daisy's Changes

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -2,21 +2,22 @@
     <v-container class="fill-height" fluid>
         <v-row justify="center">
             <v-col cols="12">
-                <h1 class="text-h2" style="text-align: center">ChatGPT Demo</h1>
+                <h1 class="text-h2" style="text-align: center; color: darkcyan; font-family: sans-serif; font-weight: lighter;">ChatGPT Demo</h1>
             </v-col>
         <v-col cols="12" style="display: flex; flex-direction: column; align-items: center">
             <v-btn
                 variant="outlined"
                 class="text-h6"
-                style="min-height: 60px; min-width: 200px"
+                style="min-height: 60px; min-width: 200px; color: teal; border-color: black; border-width: 1.5px; margin-bottom: 10px;"
                 to="/instructions"
                 >
                     Instructions
                 </v-btn>
+
                 <v-btn
                 variant="outlined"
                 class="text-h6"
-                style="min-height: 60px; min-width: 200px"
+                style="min-height: 60px; min-width: 200px; color: teal; border-color: black; border-width: 1.5px;"
                 to="/chatgpt"
                 >
                     ChatGPT Demo
